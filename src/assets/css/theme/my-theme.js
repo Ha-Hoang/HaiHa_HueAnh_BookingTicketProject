81741f754784ce3a6370fa3e0998da2c5bd31b1a
Ha-Hoang1 <hoangthuyhaiha@gmail.com>
2021-06-14 13:21:02 +0700

    set up

---
+++ src/assets/css/theme/my-theme.js
@@ -0,0 +1,15 @@
+import { createMuiTheme } from "@material-ui/core";
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#fb4226",
+    },
+    secondary: {
+      main: "#00ac4d",
+    },
+  },
+  typography: {
+    fontFamily: "Roboto",
+  },
+});
